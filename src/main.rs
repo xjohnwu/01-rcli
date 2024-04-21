30d5a31986c63d204d9ef0f3bc90db609b3bc8ae
Han Wu <xjohnwu@gmail.com>
2024-04-21 16:29:55 +0800

    support generic csv to json and yaml

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,18 @@
 // rcli csv -i input.csv -o output.json --header -d ','
 use clap::Parser;
 use rcli::{process_csv, Opts, Subcommand};
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player {
-    name: String,
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
 
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     println!("{:?}", opts);
     match opts.cmd {
-        Subcommand::Csv(opts) => process_csv(&opts.input, &opts.output),
+        Subcommand::Csv(opts) => {
+            let output = if let Some(output) = opts.output {
+                output.clone()
+            } else {
+                format!("output.{}", opts.format)
+            };
+            process_csv(&opts.input, output, opts.format)
+        }
     }
 }
--- src/opts.rs
+++ src/opts.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
-use std::path::Path;
+use core::fmt;
+use std::{path::Path, str::FromStr};
 
 #[derive(Debug, Parser)]
 #[command(name= "rcli", version, author, about, long_about = None)]
@@ -14,13 +15,22 @@ pub enum Subcommand {
     Csv(CsvOpts),
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
+
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
     #[arg(short, long, value_parser = verify_input_file)]
     pub input: String,
 
-    #[arg(short, long, default_value = "output.json")]
-    pub output: String,
+    #[arg(short, long)]
+    pub output: Option<String>,
+
+    #[arg(long, default_value = "json", value_parser = parse_output_format)]
+    pub format: OutputFormat,
 
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
@@ -34,5 +44,35 @@ fn verify_input_file(filename: &str) -> Result<String, &'static str> {
         Ok(filename.into())
     } else {
         Err("File not found")
+    }
+}
+
+fn parse_output_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse::<OutputFormat>()
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+    fn from_str(format: &str) -> Result<Self, Self::Err> {
+        match format.to_lowercase().as_str() {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => Err(anyhow::anyhow!("Invalid output format")),
+        }
+    }
+}
+
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }
